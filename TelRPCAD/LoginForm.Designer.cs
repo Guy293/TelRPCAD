db4ae77c4b07c30d786cf30535b8434424ce196e
Guy293 <35490725+Guy293@users.noreply.github.com>
2018-07-19 20:57:15 +0300

    Login Form

---
+++ TelRPCAD/LoginForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TelRPCAD
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserIDFiled = new System.Windows.Forms.TextBox();
+            this.LogInBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":שם משתמש";
+            this.label1.UseWaitCursor = true;
+            // 
+            // UserIDFiled
+            // 
+            this.UserIDFiled.Location = new System.Drawing.Point(33, 57);
+            this.UserIDFiled.Name = "UserIDFiled";
+            this.UserIDFiled.Size = new System.Drawing.Size(100, 20);
+            this.UserIDFiled.TabIndex = 1;
+            this.UserIDFiled.UseWaitCursor = true;
+            // 
+            // LogInBtn
+            // 
+            this.LogInBtn.Location = new System.Drawing.Point(47, 98);
+            this.LogInBtn.Name = "LogInBtn";
+            this.LogInBtn.Size = new System.Drawing.Size(75, 23);
+            this.LogInBtn.TabIndex = 2;
+            this.LogInBtn.Text = "התחבר";
+            this.LogInBtn.UseVisualStyleBackColor = true;
+            this.LogInBtn.UseWaitCursor = true;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.LogInBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 133);
+            this.Controls.Add(this.LogInBtn);
+            this.Controls.Add(this.UserIDFiled);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserIDFiled;
+        private System.Windows.Forms.Button LogInBtn;
+    }
+}
+
---
+++ TelRPCAD/LoginForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace TelRPCAD
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, System.EventArgs e)
+        {
+            MaximizeBox = false;
+        }
+    }
+}
